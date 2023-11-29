0f356961a0cf7fbb891d09a8120700005d9dceff
Francisco Costa <jfcpcosta@gmail.com>
2023-11-29 16:04:10 +0000

    fix: add missing dependency to create exercise use case factory

--- src/use-cases/factories/exercises/make-create-exercise-use-case.ts
+++ src/use-cases/factories/exercises/make-create-exercise-use-case.ts
@@ -1,9 +1,12 @@
 import { PrismaExercisesRepository } from "@/repositories/prisma/prisma-exercises-repository"
+import { PrismaTargetsRepository } from "@/repositories/prisma/prisma-targets-repository"
 import { CreateExerciseUseCase } from "../../exercises/create"
 
 export function makeCreateExerciseUseCase() {
     const exercisesRepository = new PrismaExercisesRepository()
-    const makeCreateExercisesUseCase = new CreateExerciseUseCase(exercisesRepository)
+    const targetsRepository = new PrismaTargetsRepository()
+    
+    const makeCreateExercisesUseCase = new CreateExerciseUseCase(exercisesRepository, targetsRepository)
 
     return makeCreateExercisesUseCase
 }
